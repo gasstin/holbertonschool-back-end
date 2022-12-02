065e298e97810be21e4f5df22de0f0645a5e9245
gasstin <gastonl1994@gmail.com>
2022-12-02 16:43:56 +0000

    fixing task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,30 +5,31 @@
     about his/her TODO list progress.
 """
 if __name__ == '__main__':
+    """
+        task 0
+    """
     import requests
     import sys
-    import urllib
     
     
     employee_id = sys.argv[1]
     
     employee_user_id = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
-    employee_info = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    
+    employee_info = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_user_id['id'])).json()
     
     # Variables
     EMPLOYEE_NAME = employee_user_id['name']
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
-    LIST_OF_TASK = []
+    TASK_TITLE = []
     for task in employee_info:
         if employee_user_id['id'] == task['userId']:
             if task['completed']:
                 NUMBER_OF_DONE_TASKS += 1
-                LIST_OF_TASK.append(task['title'])
+                TASK_TITLE.append(task['title'])
             TOTAL_NUMBER_OF_TASKS += 1
 
     print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for line in LIST_OF_TASK:
-        print("  {}".format(line))
-
-    
+    for line in TASK_TITLE:
+        print("\t {}".format(line))
